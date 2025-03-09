c660397408ec5bb4d15a9fb7bfac371917540b5e
mubashir72 <mmubashirmemon@gmail.com>
2025-03-09 23:49:54 +0500

    files updated

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { assets } from "../assets/assets";
 
 const Collection = () => {
-  return <div>Collection</div>;
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+      {/* Filter options */}
+      <div className="min-w-60">
+        <p
+          onClick={() => setShowFilter(!showFilter)}
+          className="my-2 text-xl flex items-center cursor-pointer gap-2"
+        >
+          FILTERS
+          <img
+            src={assets.dropdown_icon}
+            className={`
+        h-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+            alt=""
+          />
+        </p>
+
+        {/* filter category */}
+        <div
+          className={`border bordeer-gray-300 pl-5 py-3 mt-6 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Men"} />
+              Men
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Women"} />
+              Women
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Kids"} />
+              Kids
+            </p>
+          </div>
+        </div>
+
+        {/* sub category filter
+         */}
+        <div
+          className={`border bordeer-gray-300 pl-5 py-3 my-5 ${
+            showFilter ? "" : "hidden"
+          } sm:block`}
+        >
+          <p className="mb-3 text-sm font-medium">TYPE</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Topwear"} />
+              Topwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Bottomwear"} />
+              Bottomwear
+            </p>
+            <p className="flex gap-2">
+              <input className="w-3 " type="checkbox" value={"Winterwear"} />
+              Winterwear
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Collection;
