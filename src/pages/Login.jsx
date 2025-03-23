8603741a35c6d9a1a837cae8064e632380bdbd65
mubashir72 <mmubashirmemon@gmail.com>
2025-03-24 01:56:54 +0500

    files updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [currentState, setCurrentState] = useState("Sign Up");
+  return (
+    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
+      <div></div>
+    </form>
+  );
 };
 
 export default Login;
